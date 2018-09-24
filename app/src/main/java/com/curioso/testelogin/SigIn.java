0d4edd2e174db657dce508d037739f78eec1df89
Mike Andrade <mikewebbr@gmail.com>
2018-09-23 22:08:35 -0300

    AppCuriosoTelaDeLogin

---
+++ app/src/main/java/com/curioso/testelogin/SigIn.java
@@ -0,0 +1,59 @@
+package com.curioso.testelogin;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SigIn extends AppCompatActivity{
+
+    Button signout;
+    private FirebaseAuth mAuth;
+    TextView username;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        mAuth = FirebaseAuth.getInstance();
+        signout = (Button)findViewById(R.id.signout);
+        username = (TextView) findViewById(R.id.tvName);
+
+        mAuth = FirebaseAuth.getInstance(); // important Call
+        //Again check if the user is Already Logged in or Not
+
+        if(mAuth.getCurrentUser() == null)
+        {
+            //User NOT logged In
+            finish();
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+
+        //Fetch the Display name of current User
+        FirebaseUser user = mAuth.getCurrentUser();
+        Log.d("LOGGED", "FirebaseUser: " + user);
+
+        if (user != null) {
+            username.setText("Welcome, " + user.getDisplayName());
+        }
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
